8ad6e4b1ea8e7d0fccb87b2ffba603ef2b6fdef9
GeorgiNzm <georginzm@gmail.com>
2018-10-18 17:33:07 +0300

    Fix sorting numbers task

--- Week03/solutions/sortNumbers.cpp
+++ Week03/solutions/sortNumbers.cpp
@@ -3,6 +3,14 @@
 
 using namespace std;
 
+void printNumbers(float* numbers) {
+    for (int i = 0; i < 3; i++) {
+        cout << *(numbers + i) << " "; //one extra space but..
+    }
+
+    cout << endl;
+}
+
 int main(int argc, char const *argv[]) {
 
     float* numbers = new float[3];
@@ -12,6 +20,10 @@ int main(int argc, char const *argv[]) {
     }
 
     sort(numbers, numbers + 3);
+
+    printNumbers(numbers);
+
+    delete [] numbers;
 
     return 0;
 }
